b698543dc1a31d6ed9a54eb4bc829fab053ffaad
Praneetha789 <shettypraneetha37@gmail.com>
2026-03-09 22:46:59 +0530

    Day 1 SQL Practice

---
+++ day1_create_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Student (
+  id INT,
+  name VARCHAR(50),
+  marks INT
+  );
+  
+  INSERT INTO Student VALUES(1, 'Ram',85);
+  INSERT INTO Student VALUES(2, 'Anu', 90);
+  
+  SELECT * FROM Student;
